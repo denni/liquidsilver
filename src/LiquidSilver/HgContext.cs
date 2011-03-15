55f641cfe53273424e81210a06a81980ec666a53
denniland <denniland@localhost>
2011-03-15 15:31:45 +0800

    [HgContext] - Removed the constructors and the IDisposable interface. The class now is static and can only be used via its static methods and delegates. - Replaced the explicit delegates with action delegates. - Updated the related unit tests.
    [HgList]
    - Added some unit tests (not all green, yet).
    
    [HgListItem]
    - Added parser methods for GUID type.

--- src/LiquidSilver/HgContext.cs
+++ src/LiquidSilver/HgContext.cs
@@ -14,101 +14,8 @@ namespace LiquidSilver
 	/// </summary>
 	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming",
 		"CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Hg")]
-	public class HgContext : DisposableBase, IDisposable
+	public static class HgContext
 	{
-		#region Constructors
-
-		/// <summary>
-		/// Creates a new non-elevated context from the current
-		/// <see cref="SPContext"/> object.
-		/// </summary>
-		/// <exception cref="System.NullReferenceException">
-		/// Thrown when the current <see cref="SPContext"/> object is null.
-		/// </exception>
-		[SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
-		public HgContext() : this(SPContext.Current.Web.Url, false) { }
-
-		/// <summary>
-		/// Creates a new context from the current <see cref="SPContext"/>
-		/// object.
-		/// </summary>
-		/// <param name="elevateContext">If true, the context will be
-		///		elevated.</param>
-		/// <exception cref="System.NullReferenceException">
-		/// Thrown when the current <see cref="SPContext"/> object is null.
-		/// </exception>
-		[SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
-		public HgContext(bool elevateContext)
-			: this(SPContext.Current.Web.Url, elevateContext) { }
-
-		/// <summary>
-		/// Creates a new context from a specified <see cref="SPSite"/> object.
-		/// </summary>
-		/// <param name="site">The <see cref="SPSite"/> object to get the
-		///		context from.</param>
-		/// <param name="elevateContext">If true, the context will be
-		///		elevated.</param>
-		[SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
-		public HgContext(SPSite site, bool elevateContext)
-			: this(site.Url, elevateContext) { }
-
-		/// <summary>
-		/// Creates a new context from a specified <see cref="SPWeb"/> object.
-		/// </summary>
-		/// <param name="web">The <see cref="SPWeb"/> object to get the
-		///		context from.</param>
-		/// <param name="elevateContext">If true, the context will be
-		///		elevated.</param>
-		[SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
-		public HgContext(SPWeb web, bool elevateContext)
-			: this(web.Url, elevateContext) { }
-
-		/// <summary>
-		/// Creates a new context from a specified URL.
-		/// </summary>
-		/// <param name="siteUrl">The site's URL to get the context
-		///		from.</param>
-		/// <param name="elevateContext">If true, the context will be
-		///		elevated.</param>
-		///	<exception cref="System.IO.FileNotFoundException">
-		///	Thrown when the Site could not be found at the specified URL.
-		///	</exception>
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design",
-			"CA1054:UriParametersShouldNotBeStrings", MessageId = "0#"),
-		SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
-		public HgContext(string siteUrl, bool elevateContext)
-		{
-			if (elevateContext)
-			{
-				using (var site = new SPSite(siteUrl))
-				{
-					this.Site = new SPSite(siteUrl, GetSystemAccountToken(site));
-				}
-			}
-			else
-			{
-				this.Site = new SPSite(siteUrl);
-			}
-
-			this.Web = this.Site.OpenWeb();
-		}
-
-		#endregion Constructors
-
-		#region Properties
-
-		/// <summary>
-		/// Gets the <see cref="SPSite"/> object from the context.
-		/// </summary>
-		public SPSite Site { get; private set; }
-
-		/// <summary>
-		/// Gets the <see cref="SPWeb"/> object from the context.
-		/// </summary>
-		public SPWeb Web { get; private set; }
-
-		#endregion Properties
-
 		#region Private Methods
 
 		/// <summary>
@@ -145,34 +52,6 @@ namespace LiquidSilver
 		#region Delegates
 
 		/// <summary>
-		/// Executes the passed code in a new non-elevated context that is based on
-		/// the current context.
-		/// </summary>
-		/// <param name="code">The code to execute.</param>
-		public static void Execute(HgContextCode code)
-		{
-			using (var context = new HgContext())
-			{
-				code(context.Site, context.Web);
-			}
-		}
-
-		/// <summary>
-		/// Executes the passed code in a new context that is based on the
-		/// current context.
-		/// </summary>
-		/// <param name="elevateContext">If true, the context will be
-		///		elevated.</param>
-		/// <param name="code">The code to execute.</param>
-		public static void Execute(bool elevateContext, HgContextCode code)
-		{
-			using (var context = new HgContext(elevateContext))
-			{
-				code(context.Site, context.Web);
-			}
-		}
-
-		/// <summary>
 		/// Executes the passed code in a new context.
 		/// </summary>
 		/// <param name="site">The <see cref="SPSite"/> object to get the
@@ -180,13 +59,11 @@ namespace LiquidSilver
 		/// <param name="elevateContext">If true, the context will be
 		///		elevated.</param>
 		/// <param name="code">The code to execute.</param>
+		[SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
 		public static void Execute(SPSite site, bool elevateContext,
-			HgContextCode code)
+			Action<SPSite, SPWeb> code)
 		{
-			using (var context = new HgContext(site, elevateContext))
-			{
-				code(context.Site, context.Web);
-			}
+			Execute(site.RootWeb, elevateContext, code);
 		}
 
 		/// <summary>
@@ -197,56 +74,68 @@ namespace LiquidSilver
 		/// <param name="elevateContext">If true, the context will be
 		///		elevated.</param>
 		/// <param name="code">The code to execute.</param>
+		[SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true)]
 		public static void Execute(SPWeb web, bool elevateContext,
-			HgContextCode code)
+			Action<SPSite, SPWeb> code)
 		{
-			using (var context = new HgContext(web, elevateContext))
+			SPSite contextSite = null;
+			try
 			{
-				code(context.Site, context.Web);
+				if (elevateContext)
+					contextSite = new SPSite(web.Url, GetSystemAccountToken(web.Site));
+				else
+					contextSite = new SPSite(web.Url);
+
+				using (SPWeb contextWeb = contextSite.OpenWeb())
+				{
+					code(contextSite, contextWeb);
+				}
+			}
+			finally
+			{
+				if (contextSite != null)
+					contextSite.Dispose();
 			}
 		}
 
 		/// <summary>
 		/// Executes the passed code in a new context.
 		/// </summary>
-		/// <param name="web">The site's URL to get the new context
+		/// <param name="siteUrl">The Site URL to get the new context
 		///		from.</param>
 		/// <param name="elevateContext">If true, the context will be
 		///		elevated.</param>
 		/// <param name="code">The code to execute.</param>
-		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design",
-			"CA1054:UriParametersShouldNotBeStrings", MessageId = "0#")]
+		[SharePointPermission(SecurityAction.LinkDemand, ObjectModel = true),
+		System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design",
+			"CA1054:UriParametersShouldNotBeStrings", MessageId = "0#"),]
 		public static void Execute(string siteUrl, bool elevateContext,
-			HgContextCode code)
+			Action<SPSite, SPWeb> code)
 		{
-			using (var context = new HgContext(siteUrl, elevateContext))
+			SPSite contextSite = null;
+			try
 			{
-				code(context.Site, context.Web);
+				contextSite = new SPSite(siteUrl);
+
+				if (elevateContext)
+				{
+					SPSite site = new SPSite(siteUrl, GetSystemAccountToken(contextSite));
+					contextSite.Dispose();
+					contextSite = site;
+				}
+
+				using (SPWeb contextWeb = contextSite.OpenWeb())
+				{
+					code(contextSite, contextWeb);
+				}
+			}
+			finally
+			{
+				if (contextSite != null)
+					contextSite.Dispose();
 			}
 		}
 
 		#endregion Delegates
-
-		#region DisposableBase Members
-
-		protected override void DisposeManagedResources()
-		{
-			Web = SafeDispose(Web);
-			Site = SafeDispose(Site);
-
-			base.DisposeManagedResources();
-		}
-
-		#endregion DisposableBase Members
 	}
-
-	/// <summary>
-	///  Represents methods that are passed to <see cref="HgContext.Execute"/>,
-	///  so that the methods can run within the context.
-	/// </summary>
-	/// <param name="site">The <see cref="SPSite"/> object from the context.</param>
-	/// <param name="web">The <see cref="SPWeb"/> object from the context.</param>
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming",
-		"CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Hg")]
-	public delegate void HgContextCode(SPSite site, SPWeb web);
 }
